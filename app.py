a401ed3a3973c5fe7073cd2b9b7b32c36c5261d2
NamitS27 <namit.s@ahduni.edu.in>
2021-10-27 20:42:23 +0530

    parking lot APIs updated

--- app.py
+++ app.py
@@ -1,37 +1,28 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from twilio.rest import Client
-import datetime
-import pyotp
 import datetime
 import json
 
-pyotp_secret_key = "8asdhaiha234bjh2"
+parking_slots = [False] * 8
 
-account_sid = 'SK6761b783847974fa88d602edbb6ded12'
-auth_token = 'dNkQGyINLyc5zzFNNWNyHJsO15a31IHI'
-client = Client(account_sid, auth_token)
-
-parking_slots = [False]*8
+fixed_OTP = 346545
 
 
 def create_app():
     app = Flask(__name__)
 
-    # @app.route('/login/')
-    # def login():
-    #     pass
-
-    @app.route('/verify-otp/')
+    @app.route("/verify-otp/")
     def verify_otp():
-        # data = request.data
-        contact_number = request.form.get('contact_number')
-        otp = request.form.get('otp')
+        contact_number = request.form.get("contact_number")
+        otp = request.form.get("otp")
 
-        if pyotp.TOTP(contact_number).verify(otp):
-            return jsonify({'status':'success'}), 200
-        return jsonify({'status':'fail'}), 200
+        # if pyotp.TOTP(contact_number).verify(otp):
+        #     return jsonify({'status':'success'}), 200
+        if otp == fixed_OTP:
+            return jsonify({"status": "success"}), 200
+        return jsonify({"status": "fail"}), 200
 
+    """
     @app.route('/send-otp/')
     def send_otp():
         contact_number = request.form.get('contact_number')
@@ -44,6 +35,7 @@ def create_app():
         smsFrom = "+917600900163"
         message = client.messages.create(from_=smsFrom,body=body,to=contact_number)
         return jsonify({'status': 'success'}), 200
+    
 
     @app.route('/send-sms/',methods=['POST'])
     def send_sms():
@@ -54,20 +46,24 @@ def create_app():
         smsFrom = "+917600900163"
         message = client.messages.create(from_=smsFrom,body=body,to=contact_number)
         return jsonify({'status': 'success'}), 200
+    """
 
-    @app.route('/update-parking-lot/')
+    @app.route("/update-parking-lot/", methods=["POST"])
     def update_parking_lot():
-        pass
+        slot_number = json.loads(request.data)["slot_number"]
+        if not parking_slots[int(slot_number)]:
+            parking_slots[int(slot_number)] = False
+        else:
+            return jsonify({"status": "Already present"}), 200
+        return jsonify({"status": "success", "message": "Parking lot updated"}), 200
 
-    @app.route('/get-parking-lot/',methods=["POST"])
+    @app.route("/get-parking-lot/", methods=["GET"])
     def get_parking_lot():
-        json_data = json.loads(request.data)
-        print(json_data)
-        return jsonify({'status': 'success'}), 200
+        return jsonify({"lotStatus": parking_slots}), 200
 
     return app
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
