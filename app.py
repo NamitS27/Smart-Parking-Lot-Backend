06fb3066cbbc401a87feb70ebcb445155f271f0e
NamitS27 <namit.s@ahduni.edu.in>
2021-10-29 17:42:29 +0530

    otp verification logic updated

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ import ssl
 from bson.json_util import dumps, CANONICAL_JSON_OPTIONS
 import re
 from dotenv import load_dotenv, find_dotenv
-
-parking_slots = [False] * 8
-
-fixed_OTP = 346545
+import random
 
 load_dotenv(find_dotenv())
 
@@ -22,29 +19,44 @@ def create_app():
     password = os.environ.get('PASSWORD')
     MONGO_URI = f"mongodb+srv://{user}:{password}@iot.yjg6l.mongodb.net/{database}?retryWrites=true&w=majority"
     try:
-        mongo = pm.MongoClient(MONGO_URI,ssl_cert_reqs=ssl.CERT_NONE)
+        mongo = pm.MongoClient(MONGO_URI, ssl_cert_reqs=ssl.CERT_NONE)
         db = mongo.parking_lot
-        collection = db.parking_lot
+        parking_lot_collection = db.parking_lot
+        otp_collection = db.otp
     except Exception as e:
         print(e)
 
-    @app.route("/verify-otp/",methods=["POST"])
+    @app.route("/verify-otp/", methods=["POST"])
     def verify_otp():
         data = json.loads(request.data)
         contact_number = data["contact_number"]
         otp = data["otp"]
-
+        otpDB = None
         # if pyotp.TOTP(contact_number).verify(otp):
         #     return jsonify({'status':'success'}), 200
-        
-        slots = collection.find({"isPresent":True})
-        slots = list(slots)
-        
-        if otp == fixed_OTP and len(slots) < 8:
+        try:
+            slots = parking_lot_collection.find({"isPresent": True})
+            slots = list(slots)
+            otpDB = otp_collection.find_one(
+                {}, {"otp": 1, "generatedTimestamp": 1, "_id": 0})
+            otp_to_verify = otpDB["otp"]
+            timestamp = otpDB["generatedTimestamp"]
+        except Exception as e:
+            print(e)
+            return jsonify({'status': 'failure'}), 400
+
+        if verify_otp_with_database(otp, otp_to_verify, timestamp) and len(slots) < 8:
+            otp_collection.delete_one({"otp": otp_to_verify})
             return jsonify({"status": "success"}), 200
 
-        message = "Invalid OTP" if otp!=fixed_OTP else "No slots available"
+        message = "Invalid OTP / OTP Expired" if otp != fixed_OTP else "No slots available"
         return jsonify({"status": "failure", "message": message}), 200
+
+    def verify_otp_with_database(otp, otpDB, timestamp):
+        if otp == otpDB:
+            if (datetime.datetime.now() - timestamp).total_seconds() < 300:
+                return True
+        return False
 
     """
     @app.route('/send-otp/')
@@ -77,20 +89,32 @@ def create_app():
         slot_number = request.args.get("slot_number")
         try:
             slot_number = int(slot_number)
-            updated_result = db.parking_lot.update_one({"slot":slot_number}, {"$set" : {"isPresent": True}})
+            updated_result = db.parking_lot.update_one(
+                {"slot": slot_number}, {"$set": {"isPresent": True}})
             if updated_result.modified_count < 1:
-                return jsonify({"status": "success", "message":"Car is already present"}), 200
+                return jsonify({"status": "success", "message": "Car is already present"}), 200
         except:
             return jsonify({"status": "Update Failed"}), 200
         return jsonify({"status": "success", "message": "Parking lot updated"}), 200
 
     @app.route("/get-parking-lot/", methods=["GET"])
     def get_parking_lot():
-        slots = collection.find({},{"_id": False})
+        slots = parking_lot_collection.find({}, {"_id": False})
         return jsonify({"status": "success", "slots": list(slots)}), 200
 
+    @app.route("/generate-otp/", methods=["GET"])
+    def generate_otp():
+        otp = generate_random_otp()
+        timestamp = datetime.datetime.now()
+        otp_collection.insert_one(
+            {"otp": otp, "generatedTimestamp": timestamp})
+        return jsonify({"status": "success", "otp": otp}), 200
+
+    def generate_random_otp():
+        return random.randint(100000, 999999)
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
